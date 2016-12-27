741aab5749d2e0a54d3818f49528c893e86b62a1
Piotr Wittchen <piotr@wittchen.biz.pl>
2016-12-27 18:50:00 +0100

    updating install.sh and uinstall.sh scripts

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 set -e
 echo "installing python dependencies"
 if type "pip" > /dev/null; then
-  pip install -r requirements.txt
+  sudo pip install -r requirements.txt
 elif type "pip3" > /dev/null; then
-  pip3 install -r requirements.txt
+  sudo pip3 install -r requirements.txt
 fi
 echo "copying scripts and configuration"
-cp .pkup.conf ~/
-cp pkup_doc.py /usr/local/bin/
-cp pkup /usr/local/bin/
+sudo cp .pkup.conf ~/
+sudo cp pkup_doc.py /usr/local/bin/
+sudo cp pkup /usr/local/bin/
 echo "PKUP scripts installed successfully"
 echo "YOUR ACTION REQUIRED: Please edit ~/.pkup.conf file to add your personal configuration and reload shell"
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
+UNINSTALLED=false
+
 echo "Uninstalling pkup scripts"
-sudo rm ~/.pkup.conf 
-sudo rm /usr/local/bin/pkup_doc.py
-sudo rm /usr/local/bin/pkup
-echo "pkup scripts wer successfully uninstalled"
+if [ -s "~/.pkup.conf" ] ; then
+  sudo rm ~/.pkup.conf
+  UNINSTALLED=true
+fi
+
+if [ -s "/usr/local/bin/pkup_doc.py" ] ; then
+  sudo rm /usr/local/bin/pkup_doc.py
+  UNINSTALLED=true
+fi
+
+if [ -s "/usr/local/bin/pkup" ] ; then
+  sudo rm /usr/local/bin/pkup
+  UNINSTALLED=true
+fi
+
+if [ "$UNINSTALLED" = false ] ; then
+  echo "there was nothing to uninstall"
+else
+  echo "pkup scripts were successfully uninstalled"
+fi
